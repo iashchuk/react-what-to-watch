ebf648c530af19bf75282a2a30d8bdc384e48128
iashchuk <221box@list.ru>
2019-10-19 01:49:42 +0300

    Добавляет моковые данные с фильмами

---
+++ src/api/mocks.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    imageSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    id: 2,
+    title: `Bohemian Rhapsody`,
+    imageSrc: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    id: 3,
+    title: `Macbeth`,
+    imageSrc: `img/macbeth.jpg`
+  },
+  {
+    id: 4,
+    title: `Aviator`,
+    imageSrc: `img/aviator.jpg`
+  },
+  {
+    id: 5,
+    title: `We need to talk about Kevin`,
+    imageSrc: `img/we-need-to-talk-about-kevin.jpg`
+  },
+  {
+    id: 6,
+    title: `What We Do in the Shadows`,
+    imageSrc: `img/what-we-do-in-the-shadows.jpg`
+  },
+  {
+    id: 7,
+    title: `Revenant`,
+    imageSrc: `img/revenant.jpg`
+  },
+  {
+    id: 8,
+    title: `Johnny English`,
+    imageSrc: `img/johnny-english.jpg`
+  },
+  {
+    id: 9,
+    title: `Shutter Island`,
+    imageSrc: `img/shutter-island.jpg`
+  },
+  {
+    id: 10,
+    title: `Pulp Fiction`,
+    imageSrc: `img/pulp-fiction.jpg`
+  },
+  {
+    id: 11,
+    title: `No Country for Old Men`,
+    imageSrc: `img/no-country-for-old-men.jpg`
+  },
+  {
+    id: 12,
+    title: `Snatch`,
+    imageSrc: `img/snatch.jpg`
+  }
+];
