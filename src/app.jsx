e5693e37bdebd56a4773cc861f74dadd2aeab5df
iashchuk <221box@list.ru>
2019-10-21 23:10:45 +0300

    Добавляет черновик страницы DetailsPage

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 import MainPage from "./pages/main-page/main-page";
+import DetailsPage from "./pages/details-page/details-page";
+
+const getPage = () => {
+  switch (location.pathname) {
+    case `/details`:
+      return <DetailsPage />;
+    default:
+      return <MainPage />;
+  }
+};
 
 const App = () => {
-  return <MainPage />;
+  return getPage();
 };
 
 export default App;
