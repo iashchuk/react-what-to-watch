42365ca978a540de4bcf47616cb201a2bee61e00
iashchuk <221box@list.ru>
2019-10-26 19:43:27 +0300

    Добавляет компонент CardManage

---
+++ src/components/card-manage/card-manage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import PlayButton from "../../components/buttons/play-button";
+import PlusButton from "../../components/buttons/plus-button";
+import Button from "../../components/buttons/button";
+
+const CardManage = () => {
+  return (
+    <div className="movie-card__desc">
+      <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+      <p className="movie-card__meta">
+        <span className="movie-card__genre">Drama</span>
+        <span className="movie-card__year">2014</span>
+      </p>
+      <div className="movie-card__buttons">
+        <PlayButton className="movie-card__button" />
+        <PlusButton className="movie-card__button" isAdded={true} />
+        <Button className="movie-card__button">Add review</Button>
+      </div>
+    </div>
+  );
+};
+
+export default CardManage;
