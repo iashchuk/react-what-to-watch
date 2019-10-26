252d9c87f56c2aa3e2a66c069b7cb8a82b37ebfe
iashchuk <221box@list.ru>
2019-10-27 00:07:43 +0300

    Добавляет компонент MoviesList

--- src/components/movies-list/movie-card/movie-card.e2e.test.js
+++ src/components/movies-list/movie-card/movie-card.e2e.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import MovieCard from "./movie-card.jsx";
+import MovieCard from "./movie-card";
 
 Enzyme.configure({ adapter: new Adapter() });
 
