15c9986c0e2f802b891fbbe75a2acac1024bb83b
iashchuk <221box@list.ru>
2019-10-26 19:44:00 +0300

    Обновляет структуру страницы MainPage

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import SvgSprite from "../../components/svg-sprite/svg-sprite";
 import Catalog from "../../modules/catalog/catalog";
-import Intro from "../../modules/intro/intro";
+import MovieCardBackground from "../../components/movie-card-background/movie-card-background";
+import CardManage from "../../components/card-manage/card-manage";
+import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
+import Poster from "../../components/poster/poster";
 
 const MainPage = () => {
   return (
     <>
       <h1 className="visually-hidden">WTW</h1>
       <SvgSprite />
-      <Intro />
+      <section className="movie-card">
+        <MovieCardBackground />
+        <Header />
+        <div className="movie-card__wrap">
+          <div className="movie-card__info">
+            <Poster />
+            <CardManage />
+          </div>
+        </div>
+      </section>
       <div className="page-content">
         <Catalog />
         <Footer />
