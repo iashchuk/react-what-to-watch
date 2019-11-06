0a91ed354135d8784b3fb05ecb243d27143d16b0
iashchuk <221box@list.ru>
2019-11-06 22:45:58 +0300

    Оптимизирует каталог. Выделяет useFilter

--- src/modules/catalog/catalog.jsx
+++ src/modules/catalog/catalog.jsx
@@ -1,35 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { array } from "prop-types";
 import ShowMoreButton from "../../components/buttons/show-more-button/show-more-button";
 import MoviesFilter from "../../components/movies-filter/movies-filter";
 import MoviesList from "../../components/movies-list/movies-list";
-import { GenresMap } from "../../api/transform/transformMovies";
-
-const DEFAULT_LIST_SIZE = 6;
-const ADDED_COUNT_TO_LIST = 4;
+import { useFilter } from "./useFilter";
 
 const Catalog = ({ movies }) => {
-  const [list, setList] = useState([]);
-  const [listSize, setListSize] = useState(DEFAULT_LIST_SIZE);
-  const [activeFilter, setActiveFilter] = useState(GenresMap.All);
-
-  useEffect(() => {
-    const filtered =
-      activeFilter === GenresMap.All
-        ? movies
-        : movies.filter((item) => item.genreId === activeFilter);
-    setList(filtered);
-  }, [movies, activeFilter]);
+  const {
+    isShowMoreButtonVisible,
+    list,
+    activeFilter,
+    setListSize,
+    setActiveFilter
+  } = useFilter(movies);
 
   return (
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
       <MoviesFilter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
-      <MoviesList movies={list.slice(0, listSize)} />
+      <MoviesList movies={list} />
 
-      {list.length > listSize && (
+      {isShowMoreButtonVisible && (
         <div className="catalog__more">
-          <ShowMoreButton onClick={() => setListSize(listSize + ADDED_COUNT_TO_LIST)} />
+          <ShowMoreButton onClick={setListSize} />
         </div>
       )}
     </section>
---
+++ src/modules/catalog/useFilter.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { GenresMap } from "../../api/transform/transformMovies";
+
+const DEFAULT_LIST_SIZE = 6;
+const ADDED_COUNT_TO_LIST = 4;
+
+export const useFilter = (movies) => {
+  const [list, setList] = useState([]);
+  const [listSize, setListSize] = useState(DEFAULT_LIST_SIZE);
+  const [activeFilter, setActiveFilter] = useState(GenresMap.All);
+
+  useEffect(() => {
+    const filtered =
+      activeFilter === GenresMap.All
+        ? movies
+        : movies.filter((item) => item.genreId === activeFilter);
+    setList(filtered);
+  }, [movies, activeFilter]);
+
+  const handleSetListSize = () => {
+    setListSize(listSize + ADDED_COUNT_TO_LIST);
+  };
+
+  return {
+    isShowMoreButtonVisible: list.length > listSize,
+    list: list.slice(0, listSize),
+    activeFilter,
+    setListSize: handleSetListSize,
+    setActiveFilter
+  };
+};
