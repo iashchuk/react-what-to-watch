229dd5b40e72c057fc9aff478410357efb5bc51d
iashchuk <221box@list.ru>
2019-12-07 00:22:18 +0300

    Обновляет тесты

--- src/components/movies-filter/movies-filter.test.js
+++ src/components/movies-filter/movies-filter.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import MoviesFilter from "./movies-filter.jsx";
-import { GenresMap } from "../../api/transform/transformMovies";
+import { GenresMap } from "../../api/transform/transform-movie";
 
 describe(`MoviesFilter`, () => {
   it(`renders correctly`, () => {
-    const dom = <MoviesFilter activeFilter={GenresMap.All} setActiveFilter={(f) => f} />;
+    const dom = (
+      <MoviesFilter activeFilter={GenresMap.All} setActiveFilter={(f) => f} />
+    );
     const component = renderer.create(dom);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
